debec68777049f6e0d5729b82a0d9eb117e6d230
Hillel Coren <hillelcoren@gmail.com>
2020-07-27 17:30:46 +0300

    Test update

--- lib/invoice_ninja.dart
+++ lib/invoice_ninja.dart
@@ -1,14 +1,23 @@
 library invoice_ninja;
 
 class InvoiceNinja {
+  static String _url = 'https://app.invoiceninja.com';
   static String _token;
-  static bool _isDebug;
+  static bool _isDebug = false;
+
+  static void initialize(String token, {bool isDebug = false, String url}) {
+    _token = token;
+    _isDebug = isDebug;
+    _url ??= url;
+  }
+
+  static void setURL(String value) => _url = value;
 
   static void setToken(String value) => _token = value;
 
-  static void enableDebug() => _isDebug = true;
+  static void setDebug(bool value) => _isDebug = true;
 
   void loadProducts() {
-    print('Working... $_token $_isDebug');
+    print('Working... $_url $_token $_isDebug');
   }
 }
