952fd089f25b0a162c2e696970a87de180a87557
Hillel Coren <hillelcoren@gmail.com>
2020-07-28 20:52:59 +0300

    Tests

--- test/invoice_ninja_test.dart
+++ test/invoice_ninja_test.dart
@@ -1,6 +1,8 @@
 // Import the test package and Counter class
 import 'package:flutter_test/flutter_test.dart';
 import 'package:invoice_ninja/invoice_ninja.dart';
+import 'package:invoice_ninja/models/client.dart';
+import 'package:invoice_ninja/models/invoice.dart';
 
 void main() {
   setUp(() async {
@@ -11,9 +13,43 @@ void main() {
     );
   });
 
-  test('Load product list', () async {
+  test('Test products', () async {
     final products = await InvoiceNinja.products.load();
     expect(products.isNotEmpty, true);
+
+    final productKey = products.first.productKey;
+    final product = await InvoiceNinja.products.findByKey(productKey);
+
+    expect(productKey, product.productKey);
   });
 
+  test('Test clients', () async {
+    var testEmail = '${DateTime.now().millisecondsSinceEpoch}@example.com';
+    var client = Client.forContact(email: testEmail);
+
+    client = await InvoiceNinja.clients.save(client);
+    expect(client.id.isNotEmpty, true);
+    expect(client.contacts.first.email, testEmail);
+
+    final key = client.key;
+    client = await InvoiceNinja.clients.findByKey(key);
+    expect(client.key, key);
+    expect(client.contacts.first.email, testEmail);
+  });
+
+  test('Test invoices', () async {
+    final products = await InvoiceNinja.products.load();
+    var testEmail = '${DateTime.now().millisecondsSinceEpoch}@example.com';
+    var client = Client.forContact(email: testEmail);
+    client = await InvoiceNinja.clients.save(client);
+    expect(client.id.isNotEmpty, true);
+
+    var invoice = Invoice.forClient(client, products: [products.first]);
+    invoice = await InvoiceNinja.invoices.save(invoice);
+    expect(invoice.id.isNotEmpty, true);
+
+    final key = invoice.key;
+    invoice = await InvoiceNinja.invoices.findByKey(key);
+    expect(invoice.key, key);
+  });
 }
