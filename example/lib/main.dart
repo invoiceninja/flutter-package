8059fd410f15c14469f7dd6f004fa3fadd24aedb
Hillel Coren <hillelcoren@gmail.com>
2020-07-28 22:12:39 +0300

    Rename

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:invoiceninja/invoiceninja.dart';
+import 'package:invoiceninja/invoice_ninja.dart';
+//import 'package:invoiceninja/invoiceninja.dart';
 import 'package:invoiceninja/models/client.dart';
 import 'package:invoiceninja/models/invoice.dart';
 import 'package:invoiceninja/models/product.dart';
